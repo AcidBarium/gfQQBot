19fa757c1cb0dfe770a3d0b3c63f6f04c1aef64f
Acidbarium <acidbarium@163.com>
2025-03-27 22:27:13 +0800

    增加了喵喵help

---
+++ mmHelp.py
@@ -0,0 +1,4 @@
+def get_help():
+    str_help = "喵喵聊天功能:\n   喵喵：接入一个32B的deepseek模型，完成一些猫娘对话\n   喵喵喵：接入一个7B的deepseek模型，完成一些简单的猫娘对话\n喵喵娱乐功能:\n   喵喵gal：从https://sticker.kungal.com 抽取一张gal表情包\n喵喵学习功能:\n   喵喵home：返回大连理工大学大二下naosi课程资料根目录列表（以txt形式返回）\n   喵喵ls [num] ：返回大连理工大学大二下naosi课程资料二级目录下的文件列表（以txt形式返回）\n   喵喵head [num] ：查询以num开始的10个文件的名字\n   喵喵search [file] ：查询相关文件及其位置\n   喵喵apt [num] :下载num代表的文件\n   喵喵学习random ：从资料库里面随机抽一个资料给你去学习"
+    
+    return str_help
