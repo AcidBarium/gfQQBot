0629c5ad4ccff81b94cf4187f769d07a7a7076b6
Acidbarium <acidbarium@163.com>
2025-03-28 18:06:46 +0800

    改进mmpt的资料库和喵喵help

--- LuoLiPicture.py
+++ LuoLiPicture.py
@@ -44,12 +44,17 @@ def getLuoLiPicture(input_string):
         tags = [tags]  # 如果输入是字符串，转换为列表
     elif not isinstance(tags, list):
         return "标签必须是字符串或列表"
+    
+    uids = [1960050, 1878082, 9212166, 23098486, 25760573, 671593,1023317,7324626]
+
+    random.shuffle(uids)
 
     params = {
         "r18": 0,  # 0 = 非R18, 1 = R18, 2 = 混合
         "num": 1,  # 请求 1 张图片
         "tag": tags,  # 传递标签列表
-        "size": "small"
+        "size": "original",
+        "uid" : uids,
     }
 
     # 发送请求获取图片信息
@@ -59,7 +64,7 @@ def getLuoLiPicture(input_string):
     print(data)  # 调试信息
 
     if "data" in data and len(data["data"]) > 0:
-        img_url = data["data"][0]["urls"]["small"]  # 获取图片URL
+        img_url = data["data"][0]["urls"]["original"]  # 获取图片URL
         print(f"图片URL: {img_url}")
         return img_url
     else:
